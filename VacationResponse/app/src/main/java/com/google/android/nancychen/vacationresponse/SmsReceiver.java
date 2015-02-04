9bb53f99eaa96ea35e826518b616b8cac5d47f57
ncheneer <nancychen@nancychen-macbookpro.roam.corp.google.com>
2015-02-03 16:06:30 -0800

    Accommodate lower API level, add switch
    - enable/disable switch
    - remove method only
    available in API level 19 and up, and use lower API level-compatible
    methods

--- VacationResponse/app/src/main/java/com/google/android/nancychen/vacationresponse/SmsReceiver.java
+++ VacationResponse/app/src/main/java/com/google/android/nancychen/vacationresponse/SmsReceiver.java
@@ -4,29 +4,47 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.provider.Telephony;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
+import android.util.Log;
 
 /**
  * Created by nancychen on 1/30/15.
  */
-public class SmsReceiver extends BroadcastReceiver{
+public class SmsReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        // Get a list of incoming messages
-        SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
-
         // retrieve the default vacation response
         SharedPreferences pref = context.getSharedPreferences(
                 VacationResponseActivity.VACATION_RESPONSE_PREFERENCE,
                 Context.MODE_PRIVATE);
+
+        Boolean enabled = pref.getBoolean(
+                VacationResponseActivity.VACATION_RESPONSE_ENABLED, false);
+
+        Log.i("VacationResponseActivity", "Checked: " + String.valueOf(enabled));
+        if (!enabled) {
+            return;
+        }
         String response = pref.getString(VacationResponseActivity.SAVED_VACATION_RESPONSE, null);
-        if (response != null) {
-            for (SmsMessage message : messages) {
-                String sender = message.getOriginatingAddress();
-                SmsManager.getDefault().sendTextMessage(sender, null, response, null, null);
+        if (response == null) {
+            return;
+        }
+
+        // First get a list of the SMS messages
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            // get sms objects
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            if (pdus.length == 0) {
+                return;
             }
+
+            // large message might be broken into many but will have the same originating address.
+            String sender = SmsMessage.createFromPdu((byte[]) pdus[0]).getOriginatingAddress();
+            SmsManager.getDefault().sendTextMessage(sender, null, response, null, null);
         }
     }
 }
--- VacationResponse/app/src/main/java/com/google/android/nancychen/vacationresponse/VacationResponseActivity.java
+++ VacationResponse/app/src/main/java/com/google/android/nancychen/vacationresponse/VacationResponseActivity.java
@@ -6,49 +6,60 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Switch;
 
 public class VacationResponseActivity extends ActionBarActivity {
     public static final String SAVED_VACATION_RESPONSE = "saved_vacation_response";
+    public static final String VACATION_RESPONSE_ENABLED = "vacation_response_enabled";
     public static final String VACATION_RESPONSE_PREFERENCE = "vacation_response_preference_file";
+
+    private Switch mEnabledSwitch;
+    private SharedPreferences mVacationPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vacation_response);
+        mVacationPreferences = getSharedPreferences(
+                VACATION_RESPONSE_PREFERENCE,
+                Context.MODE_PRIVATE);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_vacation_response, menu);
+        MenuItem enableDisable = menu.findItem(R.id.enable_disable);
+        enableDisable
+                .setActionView(R.layout.enable_disable_switch)
+                .getActionView()
+                .findViewById(R.id.switchForActionBar)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences.Editor editor = mVacationPreferences.edit();
+                        editor.putBoolean(VACATION_RESPONSE_ENABLED, mEnabledSwitch.isChecked());
+                        editor.commit();
+                    }
+        });
+
+        Boolean enabled = mVacationPreferences.getBoolean(
+                VacationResponseActivity.VACATION_RESPONSE_ENABLED, false);
+        mEnabledSwitch = (Switch)
+                enableDisable.getActionView().findViewById(R.id.switchForActionBar);
+        mEnabledSwitch.setChecked(enabled);
+        Log.i("VacationResponseActivity", "Checked: "+String.valueOf(mEnabledSwitch.isChecked()));
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void saveResponse(View view) {
         String text = ((EditText) findViewById(R.id.edit_message)).getText().toString();
-        SharedPreferences pref = getSharedPreferences(
-                VACATION_RESPONSE_PREFERENCE,
-                Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = pref.edit();
+        SharedPreferences.Editor editor = mVacationPreferences.edit();
         editor.putString(SAVED_VACATION_RESPONSE, text);
         editor.commit();
 
